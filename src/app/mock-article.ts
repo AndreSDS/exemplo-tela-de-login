5dbc55eaf70084ed36a93e021aa98c8625b9b629
andresds <andre_smiths@outlook.com>
2020-04-08 01:57:03 -0300

    iniciando estilização dos componentes

--- src/app/mock-article.ts
+++ src/app/mock-article.ts
@@ -4,20 +4,47 @@ export const ARTICLES: Article[] = [
     {
         id: 1,
         title: 'My first article',
-        content: 'Este contedo pertence ao first-article',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ullam eum qui deleniti, id veritatis tempora recusandae non praesentium et. Sequi minus ducimus doloribus ex? Quam, corrupti. Laborum, corporis asperiores.',
         description: 'This is my first article',
         key: 'my-first-article',
         date: new Date(),
-        imageUrl: 'https://i7.pngguru.com/preview/620/322/538/angularjs-ruby-on-rails-typescript-web-application-icon-hacker.jpg'
+        imageUrl: 'https://t4.ftcdn.net/jpg/01/09/00/83/240_F_109008395_OQDupHMza1V6CNOzrJwWAKlaktT4IsRW.jpg'
     },
     {
         id: 2,
         title: 'My second article',
-        content: 'Este contedo pertence ao second-article',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ullam eum qui deleniti, id veritatis tempora recusandae non praesentium et. Sequi minus ducimus doloribus ex? Quam, corrupti. Laborum, corporis asperiores.',
         description: 'This is my second article',
         key: 'my-second-article',
         date: new Date(),
-        imageUrl: 'https://i7.pngguru.com/preview/620/322/538/angularjs-ruby-on-rails-typescript-web-application-icon-hacker.jpg'
+        imageUrl: 'https://t3.ftcdn.net/jpg/01/09/06/72/240_F_109067236_Lc7d3w768u7Rf9pe6dQbdXfXlXbL8ncJ.jpg'
+    },
+    {
+        id: 3,
+        title: 'My third article',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ullam eum qui deleniti, id veritatis tempora recusandae non praesentium et. Sequi minus ducimus doloribus ex? Quam, corrupti. Laborum, corporis asperiores.',
+        description: 'This is my third article',
+        key: 'my-third-article',
+        date: new Date(),
+        imageUrl: 'https://t3.ftcdn.net/jpg/01/09/40/46/240_F_109404600_9eucBKpTmlZbQN0g5ruIhybdXEj7fMvf.jpg'
+    },
+    {
+        id: 4,
+        title: 'My fourth article',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ullam eum qui deleniti, id veritatis tempora recusandae non praesentium et. Sequi minus ducimus doloribus ex? Quam, corrupti. Laborum, corporis asperiores.',
+        description: 'This is my fourth article',
+        key: 'my-fourth-article',
+        date: new Date(),
+        imageUrl: 'https://t3.ftcdn.net/jpg/01/09/46/60/240_F_109466054_0PM4NCu0FvJ9QKcD22Mqrv7UroPfwRJm.jpg'
+    },
+    {
+        id: 5,
+        title: 'My fifth article',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ullam eum qui deleniti, id veritatis tempora recusandae non praesentium et. Sequi minus ducimus doloribus ex? Quam, corrupti. Laborum, corporis asperiores.',
+        description: 'This is my fifth article',
+        key: 'my-fifth-article',
+        date: new Date(),
+        imageUrl: 'https://t3.ftcdn.net/jpg/01/09/46/76/240_F_109467678_GUi3isMXcbuEXwkhzbbL2REik6AYKToR.jpg'
     }
 ];
 
